415e5f998de7abb2d0811d72d21bff43eeeac301
王圣玉 <wangshengyu@huohua.cn>
2023-03-23 15:57:15 +0800

    feat: jsx实现

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,61 @@
+//ReactElement
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import { Type, Props, Key, Ref, ReactElement } from 'shared/ReactTypes';
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElement {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		props,
+		ref,
+		__mark: 'wang'
+	};
+
+	return element;
+};
+
+//从config中取出props创建reactElement
+//处理maybeChildren属性
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	const props: Props = {};
+	let key: Key = null;
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== 'undefined') {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== 'undefined') {
+				ref = '' + val;
+			}
+			continue;
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
